2f95345bffaeaacae98d4a818afdce08b40c44cc
charminseon@naver.com <charminseon@naver.com>
2023-12-21 17:57:17 +0900

    create repository and add projects

---
+++ java_project/test/com/jdbc/test/MockDataSource.java
@@ -0,0 +1,24 @@
+package com.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.jdbc.datasource.DataSource;
+
+public class MockDataSource extends DataSource{
+	private static MockDataSource instance = new MockDataSource();
+	
+	public static DataSource getInstace() {
+		return instance;
+	}
+	String driverName = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+	String username = "mire001";
+	String password = "mire001";
+	
+	public Connection getConnection() throws Exception{
+		Class.forName(driverName);
+		Connection conn = DriverManager.getConnection(url,username,password);
+		return conn;
+	}
+}
